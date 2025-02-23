87901fa851b4653864ec343935de09a6b001d08b
Shikhanovich, Yuriy <yshikhan@estee.com>
2025-02-23 16:07:00 -0500

    init checkin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src="./die_white_bg.jpg" id="logo" alt="logo" />
+        <h1>RtR: Random Table Roller</h1>
+        <h3>for D&D and other TTRPGs</h3>
+    </div>
+  );
+}
+
+export default App;
