de08913c794548bcfce161d7f87b4f5782f3c427
Shikhanovich, Yuriy <yshikhan@estee.com>
2025-02-23 18:40:55 -0500

    fix logo

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const IconButton = ({ to, className }) => {
   return (
     <Link to={to} className={`icon-button ${className || ''}`}>
-      <img src="./logo.svg" alt="" className="icon-button-icon" />
+      <img src="/logo.svg" alt="" className="icon-button-icon" />
       <span>Import New Table</span>
     </Link>
   );
