4b3d069b6799eb63803e2790729a724058fd2386
Shikhanovich, Yuriy <yshikhan@estee.com>
2025-03-26 19:52:25 -0400

    contact

--- src/constants/text.js
+++ src/constants/text.js
@@ -2,7 +2,9 @@ export const TEXT = {
   app: {
     title: 'RtR: Random Table Roller',
     subtitle: 'for D&D and other TTRPGs',
-    description: 'Create, manage, and roll on custom random tables for your tabletop roleplaying games. <br/> Checkout <a href="https://www.reddit.com/r/d100/" target="_blank">r/d100</a> and <a href="https://www.reddit.com/r/BehindTheTables/" target="_blank">r/BehindTheTables</a> for lots of crowd-sourced tables. <br/> See DMs Guild for my <a href="https://www.dmsguild.com/browse.php?author=Yuriy%20Shikhanovich&page=1&sort=6a&affiliate_id=3307266" target="_blank">published adventures</a>.'
+    description: 'Create, manage, and roll on custom random tables for your tabletop roleplaying games. <br/>' +
+                  'Checkout <a href="https://www.reddit.com/r/d100/" target="_blank">r/d100</a> and <a href="https://www.reddit.com/r/BehindTheTables/" target="_blank">r/BehindTheTables</a> for lots of crowd-sourced tables. <br/> See DMs Guild for my <a href="https://www.dmsguild.com/browse.php?author=Yuriy%20Shikhanovich&page=1&sort=6a&affiliate_id=3307266" target="_blank">published adventures</a>.<br/>'+
+                  'If you have any feedback or questions, reach out on <a href="https://github.com/Pyro979/rtr" target="_blank">GitHub</a > or <a href="mailto:app@uxiomatic.com">app@uxiomatic.com</a>.'
   },
   sidebar: {
     title: 'RtR: Random Table Roller',
